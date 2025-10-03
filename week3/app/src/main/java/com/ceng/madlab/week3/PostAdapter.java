cf83a82312f7b44d7ebd319b2ec6f96e14818e19
osmanyurekliaslan <osmanyurekliaslan2002@gmail.com>
2025-10-03 15:20:40 +0300

    week3_part_one
    Week-3 Lab Part One for Mobile Application Course

---
+++ week3/app/src/main/java/com/ceng/madlab/week3/PostAdapter.java
@@ -0,0 +1,55 @@
+package com.ceng.madlab.week3;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PostAdapter extends BaseAdapter {
+
+    List<Post> postList;
+    LayoutInflater inflater;
+
+    public PostAdapter(Activity activity, List<Post> postList) {
+        inflater = activity.getLayoutInflater();
+        this.postList = postList;
+    }
+
+    @Override
+    public int getCount() {
+        return postList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return postList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        View rowView;
+        rowView = inflater.inflate(R.layout.row, null);
+        EditText txtMessage = rowView.findViewById(R.id.editTextText);
+        TextView txtLocation = rowView.findViewById(R.id.txtLocation);
+        ImageView imgView = rowView.findViewById(R.id.imageView);
+
+        Post post = postList.get(position);
+        txtMessage.setText(post.getMessage());
+        imgView.setImageBitmap(post.getImage());
+        if (post.getLocation() != null){
+            txtLocation.setText(post.getLocation().getLatitude() + "" + post.getLocation().getLongitude());
+        }
+        return rowView;
+    }
+}
